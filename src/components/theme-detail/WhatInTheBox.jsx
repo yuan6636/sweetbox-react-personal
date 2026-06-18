12951c29461e0fe1d4fc850c93744417b2c66b71
yuan6636 <aleniverson777@gmail.com>
2026-06-18 18:08:58 +0800

    style(WhatInTheBox): 調整背景圖顯示方式

--- src/components/theme-detail/WhatInTheBox.jsx
+++ src/components/theme-detail/WhatInTheBox.jsx
@@ -2,7 +2,7 @@ import { featureCards } from './mockData';
 
 function WhatInTheBox() {
   return (
-    <section className="bg-neutral-400 theme-feature position-relative mt-5 mt-lg-0">
+    <section className="bg-neutral-400 theme-feature position-relative mt-5 mt-lg-0 z-1">
       <div className="container-1076 py-17 py-lg-18">
         <div className="mb-15 mb-lg-14 text-center">
           <p className="en-font fw-bold ls-1 text-primary-600 fs-7 fs-lg-6 mb-3 mb-lg-6">
@@ -30,14 +30,10 @@ function WhatInTheBox() {
           {/* features */}
           {featureCards.map((feature) => (
             <div key={feature.icon} className="detail-feature-card">
-              <div className="detail-feature-card-padding">
-                <img
-                  src={`./images/Theme_Detail/Feature/${feature.icon}.svg`}
-                  alt={feature.alt}
-                  className="mb-3"
-                />
-                <p className="text-primary-600 fw-bold fs-5 mb-3">{feature.title}</p>
-                <p className="text-neutral-800 fs-lg-8">{feature.description}</p>
+              <div className="detail-feature-card-content">
+                <img src={`./images/Theme_Detail/Feature/${feature.icon}.svg`} alt={feature.alt} />
+                <p className="text-primary-600 fw-bold fs-5">{feature.title}</p>
+                <p className="text-neutral-800 fs-8">{feature.description}</p>
               </div>
             </div>
           ))}
