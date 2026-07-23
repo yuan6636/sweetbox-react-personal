fd8e4102fcfc691afd3ee02641703621b3876ec4
yuan6636 <aleniverson777@gmail.com>
2026-07-23 12:04:29 +0800

    fix: api.ts 修正和統一為 js

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+import { getToken, logout } from './utils/auth';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const mockToken = getToken();
+
+  if (mockToken) {
+    config.headers.Authorization = `Bearer ${mockToken}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    const status = error.response?.status;
+
+    if (status === 401) {
+      // 清除 auth
+      logout();
+
+      // 導回登入頁
+      window.location.href = '/#/login';
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default api;
