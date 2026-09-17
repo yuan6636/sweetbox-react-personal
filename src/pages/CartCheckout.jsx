feedd36d019b92cf4df2df83e3f421be992f6b65
yuan6636 <aleniverson777@gmail.com>
2026-09-17 18:05:47 +0800

    fix(cart): 修正錯誤導向至 /cart

--- src/pages/CartCheckout.jsx
+++ src/pages/CartCheckout.jsx
@@ -138,7 +138,7 @@ function CartCheckout() {
   const onSubmit = async (formData) => {
     if (!enrichedCartItems || enrichedCartItems.length === 0) {
       message.warning('您的購物車裡還沒有甜點呢！');
-      navigate('/cartEmpty');
+      navigate('/cart');
       return;
     }
     // 若優惠券失效，中斷結帳
